f6769bdc4409e55956648bd4c4836584070982fe
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-09-21 23:26:17 +0530

    To CSV Add

--- upsampling.py
+++ upsampling.py
@@ -6,5 +6,5 @@ all_truck['time_y'] = pd.to_datetime(all_truck['time_y'], format = "%Y-%m-%d %H:
 all_truck = all_truck.set_index('time_y')
 seconds_resampled_data = all_truck.resample('1S').interpolate()
 
-
 print(seconds_resampled_data.head())
+seconds_resampled_data.to_csv('new_data.csv')
