dbf2c532b100cc066220ba06bb177422146c8924
Pranjal Srivastava <38811976+pranjalsrv@users.noreply.github.com>
2019-09-22 05:14:08 +0530

    upsampling updated

--- upsampling.py
+++ upsampling.py
@@ -1,10 +1,25 @@
 import pandas as pd
 
 
-all_truck = pd.read_csv('wassup.csv')
-all_truck['time_y'] = pd.to_datetime(all_truck['time'], format = "%Y-%m-%d %H:%M:%S.%f")
-all_truck = all_truck.set_index('time_y')
-seconds_resampled_data = all_truck.resample('1S').interpolate()
+all_truck = pd.read_csv('wassup_with_ti.csv')
+print(all_truck.shape)
+all_truck['time'] = pd.to_datetime(all_truck['time'], format = "%Y-%m-%d %H:%M:%S.%f")
+all_truck = all_truck.set_index('time')
 
-print(seconds_resampled_data.head())
-seconds_resampled_data.to_csv('new_data.csv')
+good_paths = [30,41,37962,27]
+good_track_points =[]
+
+for i in good_paths:
+  good_track_points.append(all_truck.loc[all_truck['track_id']==i])
+
+n = pd.concat(good_track_points)
+seconds_resampled_data = n.resample('1S').interpolate()
+
+l = []
+
+for i in good_paths:
+  l.append(seconds_resampled_data.loc[seconds_resampled_data['track_id']==i])
+
+new_l = pd.concat(l)
+
+new_l.to_csv('upscaled_new_dat_with_dir.csv')
